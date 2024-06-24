7502abdff310bebf521cd9b1a0fb66452a3d4a14
eembling <eembling@gmail.com>
2024-06-24 13:26:21 -0400

    Update rtmpauth.py

--- config/rtmpauth.py
+++ config/rtmpauth.py
@@ -1,43 +1,65 @@
 # Source: https://github.com/IRLToolkit/nginx-rtmp-auth
 
+import mysql.connector
+from mysql.connector import errorcode
 import logging
 from aiohttp import web
 import sys
-import json
 from configparser import ConfigParser
 
-configfile = '/etc/nginx/config.ini'
+configfile = '/opt/config.ini'
 config = ConfigParser()
 config.read(configfile)
 hostName = config.get('main', 'bind_to_ip')
 hostPort = config.getint('main', 'bind_to_port')
-authfile = config.get('main', 'authentication_file')
 logfile = config.get('main', 'log_to_file')
 logging.basicConfig(level=logging.INFO, handlers=[logging.FileHandler(logfile), logging.StreamHandler()])
 
-def read_auth():
-    with open(authfile) as json_file:
-        try:
-            data = json.load(json_file)
-        except json.JSONDecodeError:
-            logging.error('Could not decode JSON authentication file! Exiting...')
-            sys.exit()
-        return data
+# Database Config
+config = {
+  'user': 'username',
+  'password': 'password',
+  'host': '127.0.0.1',
+  'database': 'database_name',
+  'raise_on_warnings': True
+}
 
 async def authhandle(request):
     ipaddress = request.remote
-    logging.info('--------------------')
+    logging.info('------------------------------------------------------------------------------------')
     logging.info('Recieved new auth request from IP: ' + str(ipaddress))
-    authtable = read_auth()
     body = await request.post()
-    if 'app' in body.keys() and body['app'] in authtable.keys():
-        if body['name'] in authtable[body['app']]:
-            logging.info('Publish authenticated. (App: {}, Streamname {})'.format(body['app'], body['name']))
-            return web.Response(status=200)
-        else:
-            logging.warning('Publish failed to authenticate. (App: {}, Streamname {})'.format(body['app'], body['name']))
-            return web.Response(status=401)
-    return web.Response(status=404)
+    skey = body['name']
+    logging.info('Recieved streamkey request of ' + str(skey))
+    
+    # Attempt to connect to Database
+    try:
+       cnx = mysql.connector.connect(**config)
+       logging.info('Successfully Connected to Database')
+    except mysql.connector.Error as err:
+       if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+          logging.warning('Invalid username or password')
+       elif err.errno == errorcode.ER_BAD_DB_ERROR:
+          logging.warning('Database is invalid')
+       else:
+         logging.warning(err)
+   
+    cursor = cnx.cursor(buffered=True)
+    query = ("SELECT streamkey FROM auth WHERE streamkey = %s")
+    cursor.execute(query, (skey,))
+    dbkey = cursor.fetchall()
+    if dbkey:
+       logging.info('Publish authenticated. (App: {}, Streamname {})'.format(body['app'], body['name']))
+       #print('KEY EXISTS')
+       return web.Response(status=200)
+    else:
+       logging.warning('Publish failed to authenticate. (App: {}, Streamname {})'.format(body['app'], body['name']))
+       #print(KEY DOES NOT EXIST)
+       return web.Response(status=401)
+
+    cursor.close()
+    cnx.close()
+
 
 app = web.Application()
 app.add_routes([web.post('/auth/', authhandle)])
